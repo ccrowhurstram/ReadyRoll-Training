d7871fb5920ef4658cfedfdef939c00c4e358172
Christian Crowhurst <ccrowhurst@ramplc.com>
2018-05-10 15:25:10 +0100

    merge conflict lab - scenario 1 other changes

---
+++ Examples/src/VS2015/PartsUnlimited/Migrations/1.0.1/001_Add_Category_code.sql
@@ -0,0 +1,8 @@
+﻿-- <Migration ID="adafac65-afed-4b07-a39a-6131e5475b98" />
+GO
+
+PRINT N'Altering [dbo].[Categories]'
+GO
+ALTER TABLE [dbo].[Categories] ADD
+[Code] [nvarchar] (50) NOT NULL CONSTRAINT [DF_Categories_Code] DEFAULT (N'')
+GO
---
+++ Examples/src/VS2015/PartsUnlimited/Programmable Objects/dbo/Views/vwProducts.sql
@@ -0,0 +1,12 @@
+﻿IF OBJECT_ID('[dbo].[vwProducts]') IS NOT NULL
+	DROP VIEW [dbo].[vwProducts];
+
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+SET ANSI_NULLS ON
+GO
+CREATE VIEW [dbo].[vwProducts]
+AS
+SELECT * FROM dbo.Products
+GO
